ac059d9cb897756742b23be9a0f38737b1510837
arjun_puri <arjunpuri@arjunpuri.me>
2021-02-25 02:55:23 +0530

    version 1 completed!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+import "normalize.css";
+import "fontsource-roboto";
+
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+import Parallax from "./components/Parallax";
+import AboutMe from './components/AboutMe';
+import Introduction from "./components/Introduction";
+import ContactMe from "./components/ContactMe";
+import Projects from "./components/Projects";
+
+
+// import SkillSet from "./components/SkillSet";
+import SkillSet from "./components/SkillSet_2";
+import {skillData} from "./data/skillData";
+
+import Navbar from "./components/Navbar_2";
+
+const styles = theme => ({
+  "@global": {
+    // MUI typography elements use REMs, so you can scale the global
+    // font size by setting the font-size on the <html> element.
+    html: {
+      fontSize: 12,
+      [theme.breakpoints.up("sm")]: {
+        fontSize: 16
+      },
+      [theme.breakpoints.up("md")]: {
+        fontSize: 18
+      },
+      [theme.breakpoints.up("lg")]: {
+        fontSize: 22
+      }
+    },
+  }
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Parallax>
+        <AboutMe/>
+      </Parallax>
+      <Introduction/>
+      <SkillSet skills={skillData}/>
+      <Projects/>
+      <ContactMe/>
     </div>
   );
 }
+const Kappa = withStyles(styles)(App);
 
-export default App;
+export default Kappa;
