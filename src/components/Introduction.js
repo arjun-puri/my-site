6a714ac7efc350fe8624a6f0de6c5e74661c0493
Arjun Puri <arjun.puri@publicissapient.com>
2021-08-09 17:01:54 +0530

    added assets

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,19 +1,19 @@
-import { React } from "react";
-import { Typography, Grid, Divider } from "@material-ui/core";
+import { React } from 'react';
+import { Typography, Grid, Divider } from '@material-ui/core';
 
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-import arjun_image from "../assets/arjun_puri.jpeg";
+import arjun_image from '../assets/arjun_puri.jpeg';
 
-import "../App.css";
+import '../App.css';
 
 const useStyles = makeStyles((theme) => ({
   rootContainer: {
-      padding: theme.spacing(5),
+    padding: theme.spacing(5),
   },
   pageTitle: {},
   textContent: {
-    textAlign: "Left",
+    textAlign: 'Left',
   },
 }));
 
@@ -21,48 +21,44 @@ const Introduction = () => {
   const classes = useStyles();
 
   return (
-      <div id="introduction">
-    <Grid
-      container
-      className={classes.rootContainer}
-      spacing={4}
-      justify="center"
-      xs={11}
-    >
+    <div id='introduction'>
+      <Grid
+        container
+        className={classes.rootContainer}
+        spacing={4}
+        justify='center'
+        xs={11}
+      >
         <Grid item xs={10} className={classes.pageTitle}>
-          <Typography variant="h2" align="center" gutterBottom>
+          <Typography variant='h2' align='center' gutterBottom>
             About Me
           </Typography>
-          <Divider variant="middle" />
+          <Divider variant='middle' />
         </Grid>
 
-        <Grid container item xs={12} justify="center" id="my-image-container">
+        <Grid container item xs={12} justify='center' id='my-image-container'>
           <Grid item xs={6} sm={4} md={2}>
-            <img id="my-image" src={arjun_image} />
+            <img id='my-image' src={arjun_image} />
           </Grid>
         </Grid>
 
-          <Grid
-            item
-            xs={12}
-            className={classes.textContent}
-          >
-            <Typography variant="body2" gutterBottom>I am an alumnus of MSIT, Janakpuri which is affliated to Guru
-              Gobind Singh Indraprastha University (GGSIPU) who graduated with a B.Tech in Electronics and Communications (ECE).
-            </Typography>
+        <Grid item xs={12} className={classes.textContent}>
+          <Typography variant='body2' gutterBottom>
+            I am an alumnus of MSIT, Janakpuri which is affliated to Guru Gobind
+            Singh Indraprastha University (GGSIPU) who graduated with a B.Tech
+            in Electronics and Communications (ECE).
+          </Typography>
 
-            <Typography variant="body2" gutterBottom>
-              From an early age I've been fascinated with Electronics and
-              consequently Computers. Games like Warcraft III, Pokemon and Mario
-              formed a great part of my childhood entertainment and lead me to
-              dream that I could one day make something similar that fills
-              others with similar awe and joy as I was when I played them.
-            </Typography>
-            
-          </Grid>
+          <Typography variant='body2' gutterBottom>
+            From an early age I've been fascinated with Electronics and
+            consequently Computers. Games like Warcraft III, Pokemon and Mario
+            formed a great part of my childhood entertainment and lead me to
+            dream that I could one day make something similar that fills others
+            with similar awe and joy as I was when I played them.
+          </Typography>
         </Grid>
-        </div>    
-
+      </Grid>
+    </div>
   );
 };
 
--- src/components/SkillSet_2.js
+++ src/components/SkillSet_2.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Grid, Typography, Button, Divider } from "@material-ui/core";
+import React from 'react';
+import { Grid, Typography, Button, Divider } from '@material-ui/core';
 
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-import myResume from "../assets/my_resume.pdf";
+import myResume from '../assets/my_resume.pdf';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPager,
   faCloud,
   faLaptopCode,
   faDatabase,
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons';
 
-import SkillCard from "./SkillCard";
+import SkillCard from './SkillCard';
 
-import "../App.css";
+import '../App.css';
 
 const useStyles = makeStyles((theme) => ({
   rootContainer: {
@@ -39,34 +39,34 @@ const SkillSet = (props) => {
   });
 
   return (
-    <div id="skillset">
+    <div id='skillset'>
       <Grid
         className={classes.rootContainer}
         container
-        wrap="wrap"
-        justify="space-around"
+        wrap='wrap'
+        justify='space-around'
         spacing={4}
         xs={11}
       >
         {/* this is page title */}
-        <Grid item xs={10} className="page-title">
-          <Typography variant="h2" align="center" gutterBottom>
+        <Grid item xs={10} className='page-title'>
+          <Typography variant='h2' align='center' gutterBottom>
             Skills
           </Typography>
-          <Divider variant="middle" />
+          <Divider variant='middle' />
         </Grid>
 
         {/* a skill card */}
         {skillList}
 
         <Grid item xs={10}>
-          <Divider variant="middle" />
+          <Divider variant='middle' />
         </Grid>
 
         {/* download resume button */}
         <Grid item xs={12}>
           <a href={myResume} download="arjun's resume.pdf">
-            <Button variant="contained" color="primary" disableElevation>
+            <Button variant='contained' color='primary' disableElevation>
               Download Resume
             </Button>
           </a>
